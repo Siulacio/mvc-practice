c364a9f8ceae70bce6391393cb85ec8327ba03fe
Siulacio <siulacio@hotmail.com>
2023-05-28 21:41:59 -0500

    feat: resolving twig and Article dependencies

--- app/config.php
+++ app/config.php
@@ -1,5 +1,16 @@
 <?php
 
+use Blog\Interfaces\IArticle;
+use Blog\Persistences\InMemoryArticle;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use function DI\create;
+
 return [
+    IArticle::class => create(InMemoryArticle::class),
+    Environment::class => function () {
+        $loader = new FilesystemLoader(__DIR__ . "/../src/Blog/Views");
+        return new Environment($loader);
+    },
 
 ];
