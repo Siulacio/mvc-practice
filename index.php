a9df97428d7fd92c4ad69e17aa3e42025768494f
Siulacio <siulacio@hotmail.com>
2023-05-28 21:40:43 -0500

    feat: display reporting errors

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 use FastRoute\RouteCollector;
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 $container = require __DIR__ . '/app/bootstrap.php';
 
 $dispatcher = FastRoute\simpleDispatcher(
