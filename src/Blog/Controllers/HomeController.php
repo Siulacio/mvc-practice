fdf71ca1cdaea6b9cc9ef2fe57a0c731d98a392b
Siulacio <siulacio@hotmail.com>
2023-05-28 21:43:55 -0500

    feat: defining constructor for HomeController

--- src/Blog/Controllers/HomeController.php
+++ src/Blog/Controllers/HomeController.php
@@ -2,8 +2,19 @@
 
 namespace Blog\Controllers;
 
+use Blog\Interfaces\IArticle;
+use Twig\Environment;
+
 class HomeController
 {
+    protected IArticle $articleRepository;
+    protected Environment $twig;
+
+    public function __construct(IArticle $articleRepository, Environment $twig)
+    {
+        //
+    }
+
     public function index()
     {
         echo 'Hola desde el HomeController';
