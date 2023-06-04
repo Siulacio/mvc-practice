41e3e1c422f27e39a146b5f02bf7ac216f2f338e
Siulacio <siulacio@hotmail.com>
2023-06-04 16:05:05 -0500

    feat: process and validate register form

--- src/Application/Controllers/AuthController.php
+++ src/Application/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace Application\Controllers;
 
+use Application\Entities\User;
 use Application\Services\Doctrine;
 use Twig\Environment;
 
@@ -23,11 +24,39 @@ class AuthController
 
     public function registro()
     {
+        $errors = [];
+
         if (isset($_POST['submit'])) {
-            echo 'Hemos recibido el formulario de registro';
-            exit;
+            if (empty($_POST['username'])) {
+                $errors[] = 'El nombre de usuario es requerido';
+            }
+
+            if (empty($_POST['email'])) {
+                $errors[] = 'El correo electrónico es requerido';
+            }
+
+            if (empty($_POST['password'])) {
+                $errors[] = 'La contraseña es requerida';
+            }
+
+            if (empty($errors)) {
+                $username = filter_input(INPUT_POST, 'username');
+                $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+                $password = filter_input(INPUT_POST, 'password');
+
+                $user = new User;
+                $user->setEmail($email);
+                $user->setUsername($username);
+                $user->setPassword(password_hash($password, PASSWORD_DEFAULT));
+                $this->doctrine->em->persist($user);
+                $this->doctrine->em->flush();
+            }
+
         }
-        echo $this->twig->render('registro.twig');
+        echo $this->twig->render('registro.twig', [
+            'errors' => $errors,
+            'post' => $_POST,
+        ]);
     }
 
 }
